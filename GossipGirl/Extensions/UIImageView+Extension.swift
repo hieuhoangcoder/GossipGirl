281fc4bb11f57fb1e09db7457f6a79f781ca1e37
Kim Page <kpage989@gmail.com  git config --global user.name kimpage>
2019-02-08 21:10:07 +0700

    Image Cache

---
+++ GossipGirl/Extensions/UIImageView+Extension.swift
@@ -0,0 +1,46 @@
+//
+//  UIImageView+Extension.swift
+//  GossipGirl
+//
+//  Created by Kim Page on 2/8/19.
+//  Copyright © 2019 Kim Page. All rights reserved.
+//
+
+
+import UIKit
+
+let imageCache = NSCache<AnyObject, AnyObject>()
+
+extension UIImageView {
+    
+    func loadImageUsingCacheWithUrlString(_ urlString: String) {
+        
+        self.image = nil
+        
+        if let cachedImage = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
+            self.image = cachedImage
+            return
+        }
+        
+        let url = URL(string: urlString)
+        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+            
+            
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            DispatchQueue.main.async(execute: {
+                
+                if let downloadedImage = UIImage(data: data!) {
+                    imageCache.setObject(downloadedImage, forKey: urlString as AnyObject)
+                    
+                    self.image = downloadedImage
+                }
+            })
+            
+        }).resume()
+    }
+    
+}
